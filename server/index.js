032c315986cf1977f54b962a1ef1a67f5fe5274f
Hardik Agarwal <52121690+hardik22032000@users.noreply.github.com>
2023-07-30 14:52:58 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -22,12 +22,19 @@ database.connect();
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());
+// app.use(
+//     cors({
+//         origin:"http://localhost:3000",
+//         credentials: true
+//     })
+// )
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"*",
         credentials: true
     })
 )
+
 app.use(
     fileUpload({
         useTempFiles: true,
